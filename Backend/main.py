3fe6ef62361535b40d576857f45f814d8d1c6051
yateesh-chandra-duggirala <yateeshchandraduggirala3@gmail.com>
2024-05-03 20:28:28 +0530

    Committing the completed backend with validations added

--- Backend/main.py
+++ Backend/main.py
@@ -3,7 +3,7 @@
 # pydantic - which provides DTOs of an employee
 # Install the above libraries : pip install fastapi
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from typing import Annotated
 from pydantic import BaseModel
 import model
@@ -38,9 +38,16 @@ db_dependency = Annotated[Session, Depends(get_db)]
 # Update - Put
 # Select -  Get
 
-@app.post("/employee")
+@app.post("/employee", status_code=201)
 def create_employee(employee : EmployeeDTO, db : db_dependency):
     emp_obj = Employee(name = employee.name, email = employee.email, sal = employee.sal)
+    print(emp_obj.email)
+    email_check = db.query(Employee).filter(Employee.email == emp_obj.email).first()
+    print(email_check)
+    if email_check != None :
+        raise HTTPException(status_code=409, detail="Duplicate Email Ids not allowed")
+    if not emp_obj.email.endswith("@gmail.com"):
+        raise HTTPException(status_code=400, detail="Invalid Email")
     db.add(emp_obj)
     db.commit()
     return "Successfully added"
@@ -58,3 +65,21 @@ def get_employee(db:db_dependency):
 def get_employee_by_id(emp_id : int, db : db_dependency):
     result = db.query(Employee).filter(Employee.id == emp_id).first()
     return result
+
+@app.put("/employee/{emp_id}")
+def update_employee(emp_id : int, employee : EmployeeDTO, db : db_dependency):
+    result = db.query(Employee).filter(Employee.id == emp_id).first()
+    result.email = employee.email
+    result.name = employee.name
+    result.sal = employee.sal
+    db.commit()
+    return "Updated Successfully"
+
+@app.delete("/employee/{emp_id}")
+def delete_employee(emp_id : int, db: db_dependency):
+    result = db.query(Employee).filter(Employee.id == emp_id).first()
+    if result is None :
+        raise HTTPException(status_code=404, detail = "Concerned Employee with this id is not found")
+    db.delete(result)
+    db.commit()
+    return "Deleted Successfully"
